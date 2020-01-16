0113dbb1e1f688ab850e6fb5fa8d26f02d71968e
Iurie Guzun <iurie_guzun@yahoo.com>
2020-01-16 13:40:07 -0500

    Modified an addNote func

--- Mooskine/AppDelegate.swift
+++ Mooskine/AppDelegate.swift
@@ -17,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         dataController.load()
+        let navigationController = window?.rootViewController as! UINavigationController
+        let notebooksListViewController = navigationController.topViewController as! NotebooksListViewController
+        notebooksListViewController.dataController = dataController
         return true
     }
 
